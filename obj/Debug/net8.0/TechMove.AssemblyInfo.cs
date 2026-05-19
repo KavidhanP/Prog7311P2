84dbbb26614f8730493f095d1edbcb985df96f5a
KavidhanP <st10444513@vcconnect.edu.za>
2026-05-19 23:40:57 +0200

    TechMove GLMS - Full Project Commit

--- obj/Debug/net8.0/TechMove.AssemblyInfo.cs
+++ obj/Debug/net8.0/TechMove.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechMove")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40ea5ccc3149a352635e46556ada133069ebd1a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechMove")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechMove")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
